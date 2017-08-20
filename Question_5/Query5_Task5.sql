915ebaa67116fe71e2294d321f4e203ec62f4fe4
Dragos Bercea <Dragos@Dragoss-MacBook-Pro.local>
2017-08-20 13:25:26 +1200

    made question 5 a bit better.

--- Question_5/Query5_Task5.sql
+++ Question_5/Query5_Task5.sql
@@ -1,9 +1,15 @@
-select city,
-avg(share) as average_share
+select city, avg(share) as average_share
 from accomplices
-group by city;
+where city = 'Chicago'
+group by city
+union all
+(select city, avg(share) as average_share
+from accomplices
+where city <> 'Chicago'
+group by city order by average_share desc
+LIMIT 1);
 
 --          city         |     average_share     
 -- ----------------------+-----------------------
---  Evanston             | 8232.1666666666666667
 --  Chicago              | 4178.0909090909090909
+--  Evanston             | 8232.1666666666666667
